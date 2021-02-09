5e48bf95defe99b666b993c09bba62d2b3233f52
Steph Bench <stephane.benchimol@mfi.fr>
2021-02-10 00:48:32 +0100

    Add MeteoFrance public observation fetcher

--- datafetch/weather/meteofrance/obs/core.py
+++ datafetch/weather/meteofrance/obs/core.py
@@ -1,20 +1,49 @@
 """
 Fetch observation data from MeteoFrance public data
 """
+from datetime import datetime, timedelta
 from pathlib import Path
 from typing import Union
+
+import pydantic
 
 from datafetch.tools.http.core import SimpleHttpFetch
 
 
-class MeteoFranceObservation(SimpleHttpFetch):
-    base_url = "https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Synop/synop.2021020812.csv"
+class MeteoFranceObservationFetch(SimpleHttpFetch, pydantic.BaseModel):
+    """
+    Download MeteoFrance Observation from public dataset
+    """
+    base_url = "https://donneespubliques.meteofrance.fr/donnees_libres/"
 
-    def download_synop_for(self, datetime: str, **kwargs) -> Union[Path, None]:
+    observation_type_url_suffix = {
+        'synop': 'Txt/Synop'
+    }
+
+    def fetch(self, destination_dir: str,
+              observation_type: str = "synop", datetime_ref: str = None,
+              **kwargs) -> Union[Path, None]:
         """
         Download synop csv data for a particular datetime
-        :param datetime: eg. 2021020812
+
+        :param destination_dir:
+        :param observation_type: eg. synop
+        :param datetime_ref: eg. 2021020812
+        :param kwargs:
         :return:
         """
-        url_suffix = "Txt" / "Synop" / f"synop.{datetime}.csv"
-        return self.download_url(suffix=url_suffix, **kwargs)
+        # Handle observation type
+        if observation_type not in self.observation_type_url_suffix:
+            raise NotImplementedError(f"Observation type {observation_type} not implemented")
+        obs_type_suffix = self.observation_type_url_suffix[observation_type]
+
+        # Default datetime
+        if datetime_ref is None:
+            now = datetime.utcnow() - timedelta(days=1)
+            # TODO : get the latest synoptic hour from current time
+            datetime_ref = now.strftime("%Y%m%d00")
+
+        # Build full url suffix
+        url_suffix = "/".join([obs_type_suffix, f"{observation_type}.{datetime_ref}.csv"])
+
+        return super().fetch(destination_dir=destination_dir, url_suffix=url_suffix, **kwargs)
---
+++ tests/weather/meteofrance/test_obs.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from datafetch.tools.http.core import SimpleHttpFetch
+from datafetch.utils import FetchWithTemporaryExtensionMixin
+from datafetch.weather.meteofrance.obs.core import MeteoFranceObservationFetch
+
+
+def test_meteofrance_obs(tmp_path):
+    fetcher = MeteoFranceObservationFetch()
+    assert isinstance(fetcher, (MeteoFranceObservationFetch,
+                                SimpleHttpFetch, FetchWithTemporaryExtensionMixin))
+
+    # Fetch by default
+    r = fetcher.fetch(destination_dir=str(tmp_path))
+    assert isinstance(r, Path)
+    assert r.is_file()
+    raise Exception()
