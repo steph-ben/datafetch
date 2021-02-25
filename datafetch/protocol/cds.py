a09080035d2e8e39e5896c30c67b1f53abfbad83
Steph Bench <stephane.benchimol@mfi.fr>
2021-02-26 00:09:22 +0100

    Refactor cds and associated tests

--- datafetch/protocol/cds.py
+++ datafetch/protocol/cds.py
@@ -21,6 +21,23 @@ class ClimateDataStoreApi(SimpleHttpFetch,
     See :
         - https://github.com/ecmwf/cdsapi
         - https://github.com/ecmwf/cdsapi/blob/master/examples/example-era5-update.py
+
+        >>> cds = ClimateDataStoreApi(wait_until_complete=True)
+        >>> fp = cds.fetch(
+            cds_resource_name='reanalysis-era5-pressure-levels',
+            cds_resource_param={
+                'product_type': 'reanalysis',
+                'format': 'grib',
+                'variable': 'temperature',
+                'pressure_level': '850',
+                'year': '2021',
+                'month': '02',
+                'day': '18',
+                'time': ['00:00'],
+            },
+            destination_dir='/tmp/'
+        )
+
     """
 
     # For connecting CDS
@@ -30,8 +47,7 @@ class ClimateDataStoreApi(SimpleHttpFetch,
     _cds_internal_wait_until_complete: bool = False
     _cds: cdsapi.Client = None
 
-    # Do synchrone request or not
-    wait_until_complete: bool = False
+    use_download_db = True
 
     class Config:
         underscore_attrs_are_private = True
@@ -74,6 +90,7 @@ class ClimateDataStoreApi(SimpleHttpFetch,
     def fetch(self,
               cds_resource_name: str, cds_resource_param: dict,
               destination_dir: str, destination_filename: str = None,
+              wait_until_complete: bool = True,
               **kwargs) -> Union[Path, None]:
         """
         Fetching ...
@@ -89,7 +106,7 @@ class ClimateDataStoreApi(SimpleHttpFetch,
 
         if downdb_record.queue_id:
             state, result = self.check_queue_id_status(downdb_record.queue_id,
-                                                       wait_until_complete=self.wait_until_complete)
+                                                       wait_until_complete=wait_until_complete)
             if state == "completed":
                 url = result['location']
 
@@ -102,7 +119,22 @@ class ClimateDataStoreApi(SimpleHttpFetch,
                     record_key=downdb_record.key,
                     **kwargs)
 
-    def queue_request_if_needed(self, cds_resource_name: str, cds_resource_param: dict) -> Tuple[DownloadRecord, bool]:
+    def submit_to_queue(
+            self, cds_resource_name: str, cds_resource_param: dict
+    ) -> Union[str, Tuple[DownloadRecord, bool]]:
+        """
+        Submit a request to CDS queue
+
+        :param cds_resource_name:
+        :param cds_resource_param:
+        :return:
+        """
+        if not self.use_download_db:
+            return self._queue_request(cds_resource_name, cds_resource_param)
+        else:
+            return self._queue_request_with_db(cds_resource_name, cds_resource_param)
+
+    def _queue_request_with_db(self, cds_resource_name: str, cds_resource_param: dict) -> Tuple[DownloadRecord, bool]:
         """
         Trigger a request on CDS if needed, and record request id in a database
 
@@ -111,7 +143,7 @@ class ClimateDataStoreApi(SimpleHttpFetch,
         :param dict:
         :return:
         """
-        record_key = {'name': cds_resource_name, 'param': cds_resource_param}
+        record_key = self.get_resource_key(cds_resource_name, cds_resource_param)
         with self:
             logger.debug(f"{cds_resource_name} Checking if queuing to CDS is needed ...")
             downdb_record, created = self.db_get_record(key=str(record_key))
@@ -119,7 +151,7 @@ class ClimateDataStoreApi(SimpleHttpFetch,
                 # No request has been made yet
                 try:
                     logger.info(f"{cds_resource_name} : Making request queue to CDS ...")
-                    queue_id = self.queue_request(cds_resource_name, cds_resource_param)
+                    queue_id = self._queue_request(cds_resource_name, cds_resource_param)
 
                     if queue_id:
                         downdb_record.set_queued(queue_id)
@@ -135,7 +167,7 @@ class ClimateDataStoreApi(SimpleHttpFetch,
 
         return downdb_record, created
 
-    def queue_request(self, cds_resource_name: str, cds_resource_param: dict) -> Union[str, None]:
+    def _queue_request(self, cds_resource_name: str, cds_resource_param: dict) -> Union[str, None]:
         """
         Trigger a request on CDS
 
@@ -150,10 +182,39 @@ class ClimateDataStoreApi(SimpleHttpFetch,
         )
         return self.get_queue_id_from_result(r)
 
-    def check_queue_id_status(self, queue_id: str,
-                              wait_until_complete: bool = False,
-                              sleep_seconds: int = 10,
-                              max_try: int = 6) -> Tuple[str, dict]:
+    def check_queue(self, cds_resource_name: str, cds_resource_param: dict, **kwargs) -> Union[DownloadRecord, None]:
+        """
+        Check a queue status and update db accordingly
+
+        :param cds_resource_name:
+        :param cds_resource_param:
+        :return:
+        """
+        if not self.use_download_db:
+            logger.error(f"Cannot check queue status while not using sqlite database to track requests id")
+            return None
+
+        record_key = self.get_resource_key(cds_resource_name, cds_resource_param)
+        downdb_record, created = self.db_get_record(key=str(record_key))
+        if created:
+            logger.error(f"Cannot find existing request id for {record_key} ...")
+            return None
+
+        state, result = self.check_queue_by_id(downdb_record.queue_id, **kwargs)
+        logger.info(f"{downdb_record.queue_id} : {state}")
+        if state == "completed":
+            downdb_record.origin_url = result['location']
+            downdb_record.set_queued_and_ready()
+        if state == "failed":
+            downdb_record.set_failed(error=str(result))
+        downdb_record.save()
+
+        return downdb_record
+
+    def check_queue_by_id(self, queue_id: str,
+                          wait_until_complete: bool = False,
+                          sleep_seconds: int = 10,
+                          max_try: int = 6) -> Tuple[str, dict]:
         """
         Check status for a particular request id
 
@@ -166,8 +227,9 @@ class ClimateDataStoreApi(SimpleHttpFetch,
         r = Result(client=self.cds, reply=None)
         if not wait_until_complete:
             r.update(request_id=queue_id)
-            logger.debug(r.reply)
+            logger.info(f"{queue_id} : {r.reply['state']}")
             return r.reply['state'], r.reply
+
         else:
             nb_try = 0
             while True:
@@ -182,7 +244,8 @@ class ClimateDataStoreApi(SimpleHttpFetch,
                 if state in ("queued", "running"):
                     nb_try += 1
                     if nb_try > max_try:
-                        logger.error(f"{queue_id} : Too many attempts checking results ({nb_try} / {max_try}), exiting ...")
+                        logger.error(
+                            f"{queue_id} : Too many attempts checking results ({nb_try} / {max_try}), exiting ...")
                         return state, r.reply
 
                     logger.info(f"{queue_id} : Still in progress : {state} ... (attempt {nb_try} / {max_try})")
@@ -195,7 +258,46 @@ class ClimateDataStoreApi(SimpleHttpFetch,
 
                 raise Exception(f"Unknown API state {state} ...")
 
-    def get_queue_id_from_result(self, result: cdsapi.api.Result) -> Union[str, None]:
+    def download_result(self,
+                        cds_resource_name: str, cds_resource_param: dict,
+                        destination_dir: str, destination_filename: str = None,
+                        **kwargs) -> Union[Path, None]:
+        """
+        Download result locally
+
+        :param cds_resource_name:
+        :param cds_resource_param:
+        :param destination_filename:
+        :param destination_dir:
+        :param kwargs:
+        :return:
+        """
+        if not self.use_download_db:
+            logger.error(f"Cannot check queue status while not using sqlite database to track requests id")
+            raise ReferenceError()
+
+        record_key = self.get_resource_key(cds_resource_name, cds_resource_param)
+        downdb_record, created = self.db_get_record(key=str(record_key))
+        if created:
+            logger.error(f"Cannot find existing request id for {record_key} ...")
+            return None
+
+        if downdb_record.check_queued_and_ready():
+            return super().fetch(
+                # For SimpleHttpFetch
+                url_suffix=downdb_record.origin_url,
+                # For FetchWithTemporaryExtensionMixin
+                destination_dir=destination_dir, destination_filename=destination_filename,
+                # For DownloadedFileRecorderMixin
+                record_key=downdb_record.key,
+                **kwargs)
+        else:
+            logger.error(f"Request is not ready for download {downdb_record.queue_id}")
+            logger.debug(downdb_record.__dict__)
+            return None
+
+    @staticmethod
+    def get_queue_id_from_result(result: cdsapi.api.Result) -> Union[str, None]:
         """
         Extract the request id from a Result
 
@@ -210,4 +312,16 @@ class ClimateDataStoreApi(SimpleHttpFetch,
         else:
             return None
 
+    @staticmethod
+    def get_resource_key(cds_resource_name: str, cds_resource_param: dict) -> dict:
+        """
+        Generate a key from the CDS request
 
+        :param cds_resource_name:
+        :param cds_resource_param:
+        :return:
+        """
+        return {
+            'name': cds_resource_name,
+            'param': cds_resource_param
+        }
--- tests/protocol/test_cds.py
+++ tests/protocol/test_cds.py
@@ -21,49 +21,82 @@ cds_test_resource = {
 }
 
 
-def test_cds_queue_request(tmp_path):
-    cds = ClimateDataStoreApi(
-        db_dir=str(tmp_path)
-    )
+def test_cds_queue(tmp_path):
+    cds = ClimateDataStoreApi(db_dir=str(tmp_path))
+    db_record, created = cds.submit_to_queue(**cds_test_resource)
+    assert isinstance(db_record, DownloadRecord)
+    assert isinstance(created, bool)
+    assert db_record.queue_id
 
-    r1, created = cds.queue_request_if_needed(
-        cds_resource_name=cds_test_resource['cds_resource_name'],
-        cds_resource_param=cds_test_resource['cds_resource_param']
-    )
-    assert isinstance(r1, DownloadRecord)
-    assert r1.status == "queued"
-    assert r1.queue_id
-    assert created is True
+    cds = ClimateDataStoreApi(db_dir=str(tmp_path))
+    db_record = cds.check_queue(**cds_test_resource)
+    assert isinstance(db_record, DownloadRecord)
 
-    r2, created = cds.queue_request_if_needed(
-        cds_resource_name=cds_test_resource['cds_resource_name'],
-        cds_resource_param=cds_test_resource['cds_resource_param']
-    )
-    assert isinstance(r2, DownloadRecord)
-    assert created is False
-    assert r2 == r1
-    assert r1.queue_id == r2.queue_id
-
-    state, reply = cds.check_queue_id_status(queue_id=r2.queue_id, wait_until_complete=False)
+    cds = ClimateDataStoreApi()
+    state, reply = cds.check_queue_by_id(queue_id=db_record.queue_id)
     assert isinstance(state, str)
     assert isinstance(reply, dict)
 
-    state, reply = cds.check_queue_id_status(queue_id=r2.queue_id,
-                                             wait_until_complete=True,
-                                             sleep_seconds=1, max_try=2)
+    cds = ClimateDataStoreApi()
+    state, reply = cds.check_queue_by_id(
+        queue_id=db_record.queue_id,
+        wait_until_complete=True,
+        sleep_seconds=1,
+        max_try=2
+    )
     assert isinstance(state, str)
     assert isinstance(reply, dict)
 
-
-def test_cds_fetch(tmp_path):
-    cds = ClimateDataStoreApi(
-        db_dir=str(tmp_path),
-        wait_until_complete=True
-    )
-    fp = cds.fetch(
-        cds_resource_name=cds_test_resource['cds_resource_name'],
-        cds_resource_param=cds_test_resource['cds_resource_param'],
-        destination_dir=str(tmp_path)
-    )
+    cds = ClimateDataStoreApi(db_dir=str(tmp_path))
+    fp = cds.download_result(**cds_test_resource, destination_dir=str(tmp_path))
     assert isinstance(fp, Path)
-    assert fp.exists()
+    assert fp.is_file()
+
+
+#
+# def test_cds_queue_request(tmp_path):
+#     cds = ClimateDataStoreApi(
+#         db_dir=str(tmp_path)
+#     )
+#
+#     r1, created = cds.queue_request_if_needed(
+#         cds_resource_name=cds_test_resource['cds_resource_name'],
+#         cds_resource_param=cds_test_resource['cds_resource_param']
+#     )
+#     assert isinstance(r1, DownloadRecord)
+#     assert r1.status == "queued"
+#     assert r1.queue_id
+#     assert created is True
+#
+#     r2, created = cds.queue_request_if_needed(
+#         cds_resource_name=cds_test_resource['cds_resource_name'],
+#         cds_resource_param=cds_test_resource['cds_resource_param']
+#     )
+#     assert isinstance(r2, DownloadRecord)
+#     assert created is False
+#     assert r2 == r1
+#     assert r1.queue_id == r2.queue_id
+#
+#     state, reply = cds.check_queue_id_status(queue_id=r2.queue_id, wait_until_complete=False)
+#     assert isinstance(state, str)
+#     assert isinstance(reply, dict)
+#
+#     state, reply = cds.check_queue_id_status(queue_id=r2.queue_id,
+#                                              wait_until_complete=True,
+#                                              sleep_seconds=1, max_try=2)
+#     assert isinstance(state, str)
+#     assert isinstance(reply, dict)
+#
+#
+# def test_cds_fetch(tmp_path):
+#     cds = ClimateDataStoreApi(
+#         db_dir=str(tmp_path),
+#         wait_until_complete=True
+#     )
+#     fp = cds.fetch(
+#         cds_resource_name=cds_test_resource['cds_resource_name'],
+#         cds_resource_param=cds_test_resource['cds_resource_param'],
+#         destination_dir=str(tmp_path)
+#     )
+#     assert isinstance(fp, Path)
+#     assert fp.exists()
