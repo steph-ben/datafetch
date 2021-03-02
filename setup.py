ac4e66298c3b3e9fcb867d32e5d29769ee26f13f
Stephane <stephane.benchimol@gmail.com>
2021-03-02 12:14:59 +0100

    Publish (#20)
    * Add setup.py stuffs
    
    * Add github action
    
    Co-authored-by: Steph Bench <stephane.benchimol@mfi.fr>

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
+from pathlib import Path
+
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+
+basedir = Path(__file__).parent
+readme_content = (basedir / "README.md").read_text()
+requirements = (basedir / "requirements.txt").read_text()
+
 
 setup(
     name="datafetch",
     version="0.0.1",
     author="steph-ben",
     author_email="stephane.benchimol@gmail.com",
-    description="Tools for fetching data, and providing ready-to-use [Prefect](https://prefect.io) flows",
+    description="Tools for fetching data, and providing ready-to-use https://prefect.io flows",
+    long_description=readme_content,
+    long_description_content_type="text/markdown",
     url="https://github.com/steph-ben/datafetch",
     packages=find_packages(),
     install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
 )
